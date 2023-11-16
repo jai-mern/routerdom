7139f10e2404bffe36390c070d9f21d4a7627742
jai-mern <Kjaya8862@gamail.com>
2023-11-16 07:31:27 +0530

    Fullstack

--- src/Fullstack.jsx
+++ src/Fullstack.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 function FullStack() {
   return (
-    <div>Fullstack</div>
+    <div>
+      <h1>Full Stack Developer</h1>
+      <img
+        src="https://blog.eduonix.com/wp-content/uploads/2018/09/Full-Stack-Developer.jpg"
+        alt="Full Stack Developer"
+      />
+    </div>
   );
 }
 
